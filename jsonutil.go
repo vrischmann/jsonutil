694784f9315ee9fc763c1d30f28753cba21307aa
Vincent Rischmann <vincent@vrischmann.me>
2015-09-20 00:09:57 +0200

    add some documentation for Duration

--- jsonutil.go
+++ jsonutil.go
@@ -6,12 +6,18 @@ import (
 	"time"
 )
 
-type Duration struct{ time.Duration }
+// Duration is a wrapper around time.Duration which implements json.Unmarshaler and json.Marshaler.
+// It marshals and unmarshals the duration as a string in the format accepted by time.ParseDuration and returned by time.Duration.String.
+type Duration struct {
+	time.Duration
+}
 
+// MarshalJSON implements the json.Marshaler interface. The duration is a quoted-string in the format accepted by time.ParseDuration and returned by time.Duration.String.
 func (d Duration) MarshalJSON() ([]byte, error) {
 	return []byte(`"` + d.String() + `"`), nil
 }
 
+// UnmarshalJSON implements the json.Unmarshaler interface. The duration is expected to be a quoted-string of a duration in the format accepted by time.ParseDuration.
 func (d *Duration) UnmarshalJSON(data []byte) error {
 	var s string
 	if err := json.Unmarshal(data, &s); err != nil {
